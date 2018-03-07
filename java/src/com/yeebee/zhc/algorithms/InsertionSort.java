3163d2e04653fedf36688583293cfd22b60b54e2
zhc <you@example.com>
2018-03-07 13:04:45 +0800

    insertion sort

---
+++ java/src/com/yeebee/zhc/algorithms/InsertionSort.java
@@ -0,0 +1,67 @@
+package com.yeebee.zhc.algorithms;
+
+import java.util.Arrays;
+/**
+ * 
+ * @author yeebee
+ *≤Â»Î≈≈–Ú
+ */
+
+public class InsertionSort {
+	
+	/**
+	 * …˝–Ú
+	 * @param a
+	 * @return
+	 */
+	public static int[] sort(int a[]) {
+		if(a.length<2)
+			return a;
+		for(int j=1;j<a.length;j++) {
+			int key=a[j];
+			//Insert a[j] into the sorted sequence a[1...j-1].
+			int i=j-1;
+			while(i>=0&&a[i]>key) {
+				a[i+1]=a[i];
+				i=i-1;
+			}
+			a[i+1]=key;
+		}
+		return a;
+	}
+	
+	/**
+	 * Ωµ–Ú
+	 * @param a
+	 * @return
+	 */
+	public static int[] sortDesc(int a[]) {
+		if(a.length<2)
+			return a;
+		for(int j=1;j<a.length;j++) {
+			int key=a[j];
+			//Insert a[j] into the sorted sequence a[1...j-1].
+			int i=j-1;
+			while(i>=0&&a[i]<key) {
+				a[i+1]=a[i];
+				i=i-1;
+			}
+			a[i+1]=key;
+		}
+		return a;
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] a=new int[]{5,2,4,6,1,3};
+		int[] a1=new int[] {31,41,59,26,41,58};
+		InsertionSort.sort(a);
+		System.out.println(Arrays.toString(a));
+		InsertionSort.sortDesc(a);
+		System.out.println(Arrays.toString(a));
+		InsertionSort.sort(a1);
+		System.out.println(Arrays.toString(a1));
+		
+	}
+
+}
